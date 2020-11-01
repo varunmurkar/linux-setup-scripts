68283d32bd8fae2dc9228d445c6de9b680752cf1
varunmurkar <varun5796@gmail.com>
2020-11-01 20:02:08 +0530

    Improvements
    - add disclaimer
    - add samba
    - add Inter typeface
    - consolidated non-critical installs
    - auto-accept for installs
    - disabled PPA for TLP since it is now in the mainline Ubuntu PPA

--- ubuntu-debian-amd-setup.sh
+++ ubuntu-debian-amd-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Disclaimer
+echo "If  using this script willy-nilly breaks your stuff, future-me or anyone else, past-me is not responsible." $'\n'$" At this time, this script is not meant to be interactive, but just quickly automate the usual tasks. Check before executing."
+
 # Basics
 echo "Installing apt-fast to speed up future package downloads"
 sudo add-apt-repository ppa:apt-fast/stable
@@ -13,14 +16,14 @@ echo "Update installed packages to make avoid conflicts and skip later downloads
 sudo apt-fast upgrade -y
 
 # Install extras for sensible usage
-sudo apt-fast install ubuntu-restricted-extras vlc ffmpeg 
+sudo apt-fast install ubuntu-restricted-extras celluloid ffmpeg synaptic htop gparted gnome-tweaks gnome-tweak-tool -y
 
 # Use Brave instead of Firefox 
 sudo apt-fast install apt-transport-https curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-fast update
-sudo apt-fast install brave-browser
+sudo apt-fast install brave-browser -y
 sudo apt purge firefox -yy
 sudo apt purge firefox-locale-en -yy
 if [ -d "/home/$USER/.mozilla" ]; then
@@ -62,8 +65,8 @@ sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 
 # Fix power management
-sudo add-apt-repository ppa:linrunner/tlp
-sudo apt-fast update
+# sudo add-apt-repository ppa:linrunner/tlp
+# sudo apt-fast update
 sudo apt-fast install tlp tlp-rdw
 sudo tlp start
 
@@ -75,7 +78,7 @@ sudo apt-fast install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drive
 
 
 # Prepare gaming specs (2/2): Install Steam and custom Proton GE
-sudo apt-fast steam
+sudo apt-fast install steam -y
 cd ~
 wget https://raw.githubusercontent.com/Termuellinator/ProtonUpdater/master/cproton.sh
 sudo chmod +x cproton.sh
@@ -83,33 +86,42 @@ sudo chmod +x cproton.sh
 
 
 ## Music
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-fast update && sudo apt-fast install spotify-client -y
 
 ## Install Git and VS Code
-sudo apt-fast install git 
+sudo apt-fast install git -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-fast install apt-transport-https
 sudo apt-fast update
-sudo apt-fast install code
-
-
-#Install general utilities
-sudo apt-fast install htop synaptic 
+sudo apt-fast install apt-transport-https code -y
 
 # Make things pretty
 echo "Installing Papirus icon set"
-sudo add-apt-repository ppa:papirus/papirus
+sudo add-apt-repository ppa:papirus/papirus -y
 sudo apt-fast install papirus-icon-theme
+
+# Install Inter typeface
+wget https://github.com/rsms/inter/releases/download/v3.15/Inter-3.15.zip
+cd Downloads
+unzip Inter-3.15.zip
+sudo cp "Inter-3.15/Inter Desktop/*" /user/share/fonts/opentype/inter
+rm -rf /Inter-3.15
+cd ../
+echo "Remember to enable Inter through the Tweak tool"
+gnome-tweaks
+
+# Enable network sharing for home networks
+sudo apt-fast install samba samba-common smbclient -y
+sudo ufw allow samba
 
 # Remove unnecessary things
 sudo apt remove apport apport-gtk
 
 ## Gotta reboot now:
-sudo apt update && sudo apt upgrade -y
+sudo apt-fast update && sudo apt-fast upgrade -y
 sudo apt autoremove && sudo apt autoclean && sudo apt clean
 echo "Remember to tweak startup items and other system settings as required"
 
